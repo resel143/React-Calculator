3786444e476fbe667958f31fdfc403160c64a8d8
Reshul <53306036+resel143@users.noreply.github.com>
2021-04-10 10:53:39 +0530

    All Calculation happns in this component

---
+++ Calc.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function sum(a,b){
+    return a+b;
+}
+
+function subkarde(a,b){
+    return a-b;
+
+}
+
+function prd(a,b){
+    return a*b;
+}
+
+function dkarde(a,b){
+    return a/b;
+}
+
+
+export {sum};
+export {prd};
+export {dkarde};
+export {subkarde};
